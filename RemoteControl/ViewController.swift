ad0d4e679f03b540e2ded3911162d8c83a82e2a2
AJ <ajpriola@gmail.com>
2015-10-06 22:02:20 -0400

    Initial Commit

---
+++ RemoteControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteControl
+//
+//  Created by AJ Priola on 10/6/15.
+//  Copyright © 2015 AJ Priola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
